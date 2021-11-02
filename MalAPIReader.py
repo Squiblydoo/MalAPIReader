3151c11048f3f149c0fb14276d2e1394360dd624
Squiblydoo <squiblydoo@pm.me>
2021-11-02 05:23:29 -0400

    Added KeyboardInterrupt
    Doesn't seem to work reliably unless being ran from Python.

--- MalAPIReader.py
+++ MalAPIReader.py
@@ -2,6 +2,8 @@ import pefile
 import requests
 import bs4
 import argparse
+import sys
+import os
 
 parser = argparse.ArgumentParser(description="Read information from MalAPI.io for WinAPI information.")
 parser.add_argument("--pe", "-p", help="Specify a PE to read. The WinAPI will be checked against MalAPI and information will be printed about the API if the information is present.")
@@ -27,14 +29,17 @@ elif (args.pe):
     print(args.pe)
     pe = pefile.PE(args.pe, fast_load=True)
     pe.parse_data_directories()
-    for entry in pe.DIRECTORY_ENTRY_IMPORT:
-        print("----", entry.dll.decode("utf-8"),"----")
-        for imp in entry.imports:
-            print(imp.name.decode("utf-8"))
-            try:
-                apiCheck(imp.name.decode("utf-8"))
-            except:
-                continue
-    print("If an API listed here was used maliciously, but its use was not listed, consider contributing information to MalAPI.io.")
+    try:
+        for entry in pe.DIRECTORY_ENTRY_IMPORT:
+            print("----", entry.dll.decode("utf-8"),"----")
+            for imp in entry.imports:
+                print(imp.name.decode("utf-8"))
+                try:
+                    apiCheck(imp.name.decode("utf-8"))
+                except:
+                    continue
+    except KeyboardInterrupt:
+        pass
+        
+    print("If an WINAPI listed here was used maliciously, but its use was not listed, consider contributing information to MalAPI.io.")
             
-    
